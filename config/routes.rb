a09900c97cf3a90b011387c0bcd6f6e2a2589981
Gabriela Ilarreta <gabrielailarreta@gmail.com>
2017-01-09 22:12:23 -0300

    Envio Mensaje

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
- # get 'home/index'
-
-  #devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :users
   root 'home#index'
 
-  resources :conversations, only: [:create]
+  devise_for :users
+
+  resources :conversations, only: [:create] do
+    member do
+      post :close
+    end
+
+    resources :messages, only: [:create]
+  end
 end
